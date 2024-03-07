f46291e6fee072e476143d038eeb2f0ededd7c00
gc4764 <gc4764@gmail.com>
2024-03-07 14:03:11 +0530

    Filterring refactored

---
+++ GCLibrary/Filter/FilterBase.cs
@@ -0,0 +1,39 @@
+﻿using GCLibrary.Filter;
+using GCLibrary.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GCLibrary.Filter
+{
+    public abstract class FilterBase
+    {
+        protected string _name;
+        protected FilterBase(string name)
+        {
+            _name = name.ToUpper();
+        }
+        protected Response GenerateResult(bool resp)
+        {
+
+            ILogger _logger = new ConsoleLogger();
+            Response response;
+
+            if (resp)
+            {
+                
+                _logger.Info($"{_name} is Success");
+                response = Response.ResponseBuilder.SetSuccess($"{_name} is Success");
+                return response;
+            }
+            _logger.Error($"{_name} is Failed");
+            response = Response.ResponseBuilder.SetError($"{_name} is Failed ");
+            return response;
+        }
+    }
+}
+
+
--- Test/Filter.cs
+++ Test/Filter.cs
@@ -2,11 +2,11 @@
 using GCLibrary.Filter;
 namespace Test
 {
-    public class Filter(int a, int b, string name) : IFilter
+    public class Filter(int a, int b, string name) : FilterBase(name) ,IFilter
     {
         private readonly int A = a;
         private readonly int B = b;
-        private string _name = name.ToUpper();
+        /*private string _name = name.ToUpper();*/
         private readonly ILogger _logger = new ConsoleLogger();
 
 
@@ -23,22 +23,7 @@ namespace Test
 
         }
 
-        private Response GenerateResult(bool resp)
-        {
-            Response response;
-
-            if (resp)
-            {
-                _logger.Info($"{_name} is successfull ");
-                 response = Response.ResponseBuilder.SetSuccess($"{_name} is successfull ");
-                return response;
-            }
-            _logger.Error($"{_name} is Failed");
-
-            response = Response.ResponseBuilder.SetError($"{_name} is Failed ");
-            return response;
-
-        }
+       
 
 
     }
